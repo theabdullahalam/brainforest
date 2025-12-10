ddac99754b78b90e6cbff7c1c8422d08b0d99ac9
Abdullah Alam <abdullahalam@protonmail.com>
2025-12-09 22:29:49 -0500

    initial commit

---
+++ eleventy.config.js
@@ -0,0 +1,72 @@
+import path from "node:path";
+import * as sass from "sass";
+import { DateTime } from "luxon";
+// import { feedPlugin } from "@11ty/eleventy-plugin-rss";
+
+export default async function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("static/fonts");
+  eleventyConfig.addPassthroughCopy("static/icons");
+  eleventyConfig.addPassthroughCopy("static/js");
+  eleventyConfig.addPassthroughCopy("media");
+
+  // sass
+  eleventyConfig.addTemplateFormats("scss");
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css",
+
+    // opt-out of Eleventy Layouts
+    useLayouts: false,
+
+    compile: async function(inputContent, inputPath) {
+      let parsed = path.parse(inputPath);
+      // Don’t compile file names that start with an underscore
+      if (parsed.name.startsWith("_")) {
+        return;
+      }
+
+      let result = sass.compileString(inputContent, {
+        loadPaths: [parsed.dir || ".", this.config.dir.includes],
+      });
+
+      // Map dependencies for incremental builds
+      this.addDependencies(inputPath, result.loadedUrls);
+
+      return async (data) => {
+        return result.css;
+      };
+    },
+  });
+
+  // human readable date
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "dd LLL yyyy",
+    );
+  });
+
+  // rss
+ //  eleventyConfig.addPlugin(feedPlugin, {
+	// 	type: "atom", 
+	// 	outputPath: "/feed.xml",
+	// 	collection: {
+	// 		name: "note", 
+	// 		limit: 20,   
+	// 	},
+	// 	metadata: {
+	// 		language: "en",
+	// 		title: "Abdullah's Notes",
+	// 		subtitle: "My digital, public commonplace book",
+	// 		base: "https://notes.theabdullahalam.com",
+	// 		author: {
+	// 			name: "Abdullah Alam",
+	// 		}
+	// 	}
+	// });
+
+
+  // absolute url
+  eleventyConfig.addFilter("toAbsoluteUrl", (url) => {
+    return new URL(url, "https://blog.theabdullahalam.com/").href
+  })
+}
+
---
+++ post/hello_world.md
@@ -0,0 +1,12 @@
+---
+title: "Hello World"
+tags: ["posts"]
+preview: "Welcome to my blog."
+layout: base.njk
+---
+# Hello World
+Hello World, welcome to my blog. I have maintained several blogs over the years, but all of them have been _about_ something, and not tied to my identity. Having recently re-read Austin Kleon's Steal Like An Artist, and the excellent Almanack of Naval Ravikant, I felt inspired to start a blog publicly available, tied to my identity as an individual. Consider this an experiment in digital vulnerability.
+
+This blog has an extremely simple structure - it is a stream of posts about anything I feel will be of interest to people. In place of a fixed "tagging" system, I invite you to see the pages that page links to as a hint toward what categories that post falls under. Like an intricate connections of thoughts, clustered only by connections, and having a one-dimensional depth. 
+
+
